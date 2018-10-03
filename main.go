9f386b60ae3dcfcd8a3f517268642a722e9eef2d
Piyush Vishwakarma <piyush2206@gmail.com>
2018-10-03 16:38:49 +0530

    Implementing read from csv, batch jobs

--- main.go
+++ main.go
@@ -2,16 +2,47 @@
 package main
 
 import (
+	"encoding/csv"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"sync"
 )
 
+func main() {
+	flgs := readFlags()
+
+	websiteURLs := readURLs(*flgs.uRLsPath)
+
+	chWebsiteStatus := checkWebsites(flgs, websiteURLs)
+
+	for resStatus := range chWebsiteStatus {
+		fmt.Println(resStatus)
+	}
+}
+
+type flags struct {
+	uRLsPath    *string
+	concurrency *int
+}
+
+func readFlags() (flgs *flags) {
+	flgs = new(flags)
+
+	flgs.uRLsPath = flag.String("u", "urls.csv", "website URLs file path")
+	flgs.concurrency = flag.Int("c", 50, "concurrent requests")
+
+	flag.Parse()
+	return
+}
+
 // Enum values of tyoe Status
 const (
-	Ok    Status = "OK"
-	NotOk Status = "NOT OK"
+	NotOk Status = iota // 0
+	Ok                  // 1
 )
 
 type (
@@ -24,37 +55,67 @@ type (
 	}
 
 	// Status will have values "OK" and "NOT OK" if website is Up or down respectively
-	Status string
+	Status int
 )
 
-func main() {
-	websiteUrls := []string{"https://www.github.com", "https://www.google.com"}
-	chWebsiteStatus := checkWebsites(websiteUrls)
-	for resStatus := range chWebsiteStatus {
-		fmt.Println(resStatus)
+func (s Status) String() (strStatus string) {
+	switch s {
+	case Ok:
+		strStatus = "OK"
+	case NotOk:
+	default:
+		strStatus = "NOT OK"
 	}
+
+	return
 }
 
 func (w *websiteStatus) String() string {
-	return fmt.Sprintf("URL: %s | Status: %v", w.URL, w.Status)
+	return fmt.Sprintf("URL: %s | Status: %s", w.URL, w.Status)
 }
 
-func checkWebsites(websiteUrls []string) (chWebsiteStatus chan *websiteStatus) {
+func readURLs(urlsPath string) (websiteURLs []string) {
+	reader, err := os.Open(urlsPath)
+	csvReader := csv.NewReader(reader)
+
+	URLs, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, URL := range URLs {
+		websiteURLs = append(websiteURLs, URL...)
+	}
+
+	return
+}
+
+func checkWebsites(flgs *flags, websiteURLs []string) (chWebsiteStatus chan *websiteStatus) {
+	chWebsiteURLs := make(chan string)
 	chWebsiteStatus = make(chan *websiteStatus)
 
 	var wg sync.WaitGroup
-	wg.Add(len(websiteUrls))
+	wg.Add(len(websiteURLs))
 
-	for _, url := range websiteUrls {
-		go func(url string) {
-			status := isUp(url)
+	go func() {
+		for _, url := range websiteURLs {
+			chWebsiteURLs <- url
+		}
+		close(chWebsiteURLs)
+	}()
 
-			chWebsiteStatus <- &websiteStatus{
-				URL:    url,
-				Status: status,
+	for i := 0; i < *flgs.concurrency; i++ {
+		go func() {
+			for url := range chWebsiteURLs {
+				status := isUp(url)
+
+				chWebsiteStatus <- &websiteStatus{
+					URL:    url,
+					Status: status,
+				}
+				wg.Done()
 			}
-			wg.Done()
-		}(url)
+		}()
 	}
 
 	go func() {
@@ -68,7 +129,7 @@ func checkWebsites(websiteUrls []string) (chWebsiteStatus chan *websiteStatus) {
 func isUp(url string) (status Status) {
 	response, err := http.Get(url)
 	if err != nil {
-		status = Status(NotOk)
+		status = NotOk
 		return
 	} else {
 		defer response.Body.Close()
@@ -77,7 +138,7 @@ func isUp(url string) (status Status) {
 			status = NotOk
 			return
 		}
-		status = Status(Ok)
+		status = Ok
 	}
 	return
 }
